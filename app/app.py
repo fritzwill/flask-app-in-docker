a206b33a770d6f54b3159c522bb4db3601605684
Will Fritz <william.g.fritz@gmail.com>
2017-06-26 21:06:02 -0400

    docker-compose implementation

--- app/app.py
+++ app/app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template
 # include request above if you want request methods
 # include render_template above
+# from werkzeug.contrib.fixers import ProxyFix
+
 app = Flask(__name__)
 
-# @app.route('/')
-# def index():
-# 	return "Flask Dockerized"
+@app.route('/')
+def index():
+	return "Flask Dockerized"
 
 ##### REQUEST METHODS #####
 # @app.route("/")
@@ -20,15 +22,17 @@ app = Flask(__name__)
 # 		return "You are probably using GET"
 
 ##### HTML TEMPLATES #####
-# @app.route("/profile/<name>")
-# def profile(name):
-# 	return render_template("profile.html", name=name)
+@app.route('/<name>')
+def profile(name):
+	return render_template("profile.html", name=name)
 
 ##### PASSSING OBJECTS #####
-@app.route("/shopping")
-def shpping():
-	food = ["Cheese", "Tuna", "Beef"]
-	return render_template("shopping.html", food=food)
-	
+# @app.route("/shopping")
+# def shpping():
+# 	food = ["Cheese", "Tuna", "Beef"]
+# 	return render_template("shopping.html", food=food)
+
+# app.wsgi_app = ProxyFix(app.wsgi_app)
+
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', port=80)
+	app.run()
